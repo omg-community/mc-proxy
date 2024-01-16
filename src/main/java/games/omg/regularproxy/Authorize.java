98fcd3d71a6f9a2bc7929cf1f9f4590b6210aed6
iminlikewithyou <iilwy@omg.games>
2024-01-16 14:06:15 -0600

    what the fuck

--- src/main/java/games/omg/regularproxy/Authorize.java
+++ src/main/java/games/omg/regularproxy/Authorize.java
@@ -3,10 +3,13 @@ package games.omg.regularproxy;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.proxy.connection.registry.DimensionInfo;
+import com.velocitypowered.proxy.protocol.packet.JoinGame;
 
 import dev.simplix.protocolize.api.Location;
 import dev.simplix.protocolize.api.Protocolize;
-import dev.simplix.protocolize.data.packets.PlayerPosition;
+import dev.simplix.protocolize.api.player.ProtocolizePlayer;
+import games.omg.regularproxy.packets.SynchronizePlayerPosition;
 
 public class Authorize {
 
@@ -15,15 +18,37 @@ public class Authorize {
     Player player = event.getPlayer();
     
     event.setResult(ServerPreConnectEvent.ServerResult.denied());
+    // logger.log(event.getPlayer().getProtocolVersion());
     sendToVoid(player);
   }
 
   public void sendToVoid(Player player) {
-    PlayerPosition playerPositionPacket = new PlayerPosition(
-      new Location(0, 0, 0, 0, 0),
-      true
-    );
+    // RespawnPacket respawnPacket = new RespawnPacket("minecraft:the_void", "minecraft:the_void", 0, (short) 0, (short) 0, false, false, false, null, null, 0, (byte) 0);
+    // Respawn respawn = new Respawn(0, 0l, (short) 0, (short) 0, "minecraft:overworld", (byte) 0, new DimensionInfo("minecraft:overworld", "minecraft:overworld", false, false), (short) 0, CompoundBinaryTag.builder().build(), null, 0);
+    JoinGame joinGame = new JoinGame();
+    joinGame.setEntityId(0);
+    joinGame.setGamemode((short) 0);
+    joinGame.setDimension(0);
+    joinGame.setDifficulty((short) 0);
+    joinGame.setIsHardcore(false);
+    joinGame.setMaxPlayers(1);
+    joinGame.setLevelType("minecraft:overworld");
+    joinGame.setViewDistance(5);
+    joinGame.setReducedDebugInfo(false);
+    joinGame.setDoLimitedCrafting(false);
+    joinGame.setDimensionInfo(new DimensionInfo("minecraft:overworld", "minecraft:overworld", false, false));
+    joinGame.setPreviousGamemode((short) 0);
+    joinGame.setSimulationDistance(5);
+    joinGame.setLastDeathPosition(null);
+    joinGame.setPortalCooldown(0);
 
-    Protocolize.playerProvider().player(player.getUniqueId()).sendPacket(playerPositionPacket);
+    SynchronizePlayerPosition synchronizePlayerPosition = new SynchronizePlayerPosition();
+    synchronizePlayerPosition.location(new Location(0d, 0d, 0d, 0f, 0f));
+    synchronizePlayerPosition.flags((byte) 0);
+    synchronizePlayerPosition.teleportId(0);
+
+    ProtocolizePlayer protocolizePlayer = Protocolize.playerProvider().player(player.getUniqueId());
+    protocolizePlayer.sendPacket(joinGame);
+    protocolizePlayer.sendPacket(synchronizePlayerPosition);
   }
 }
--- src/main/java/games/omg/regularproxy/Main.java
+++ src/main/java/games/omg/regularproxy/Main.java
@@ -5,9 +5,15 @@ import org.slf4j.Logger;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 @Plugin(
   id = "regularproxy",
@@ -36,6 +42,50 @@ public class Main {
 
   @Subscribe
   public void onInitialize(ProxyInitializeEvent event) {
+    // Protocolize.protocolRegistration().createPacket(
+    //   RespawnPacket.class,
+    //   Protocol.PLAY,
+    //   PacketDirection.CLIENTBOUND,
+    //   0x45 // not sure if this is meant to be 0x45 or simply 45 but neither work
+    // );
+
     server.getEventManager().register(this, new Authorize());
+  }
+
+  // @Subscribe
+  // public void onLoginEvent(LoginEvent event) {
+  //   Player player = event.getPlayer();
+
+  //   event.setResult(ComponentResult.denied(
+  //     Component.text("goodbye " + player.getUsername())
+  //             .color(NamedTextColor.GOLD)
+  //             .decorate(TextDecoration.BOLD)
+  //   ));
+  // }
+
+  @Subscribe
+  public void onPing(ProxyPingEvent event) {
+    ServerPing originalPing = event.getPing();
+    ServerPing.Builder pingBuilder = originalPing.asBuilder();
+
+    // Make a bold orange MOTD
+    // Component motd = Component.text("regular.place");
+    // motd = motd.style(Style.style(TextColor.color(0xFFA500), TextDecoration.BOLD));
+    // // Add more text on the next line without styles and colors and formatting
+    // motd = motd.append(Component.newline());
+    // motd = motd.append(Component.text("a regular proxy"));
+
+    Component part1 = Component.text("regular.place")
+                                .color(NamedTextColor.GOLD)
+                                .decorate(TextDecoration.BOLD);
+
+    Component part2 = Component.text("\ndays without incident: 0");
+
+    Component motd = Component.text().append(part1, part2).build();
+
+    pingBuilder.description(motd);
+
+    // Update the ping
+    event.setPing(pingBuilder.build());
   }
 }
